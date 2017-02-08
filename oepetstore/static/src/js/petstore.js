a9369c1e726c5cf7db67bc887b784b8e2a336a0e
Sitthykun LY <ly.sitthykun@gmail.com>
2017-02-08 13:50:05 +0700

    replaced openerp by odoo
    move to odoo 10

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -1,4 +1,4 @@
-openerp.oepetstore = function(instance, local) {
+odoo.oepetstore = function(instance, local) {
     var _t = instance.web._t,
         _lt = instance.web._lt;
     var QWeb = instance.web.qweb;
@@ -9,5 +9,6 @@ openerp.oepetstore = function(instance, local) {
         },
     });
 
-    instance.web.client_actions.add('petstore.homepage', 'instance.oepetstore.HomePage');
+    instance.web.client_actions.add(
+        'petstore.homepage', 'instance.oepetstore.HomePage');
 }
